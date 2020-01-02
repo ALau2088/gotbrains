c3ad6dfdbe8a3596672d1811d073db56ae03931d
ALau2088 <alau12004@yahoo.com>
2020-01-01 18:01:54 -0800

    WIP implement MongoDB

--- server/index.js
+++ server/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+
+// Or using promises
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
 app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Server Now Listening on port ${port}`));
